c98cd054d9bcfd5c85575eb0d37f61cdd46cf689
Omar Shehata <omar.sameh.shehata@gmail.com>
2024-07-22 17:36:16 -0400

    WIP

--- scripts/gpt4all.js
+++ scripts/gpt4all.js
@@ -59,6 +59,7 @@ function getVec(word) {
 
 // const sim = cosineSimilarity(getVec("diamond"), getVec("ring"))
 
+processedTweets = ['hello!', 'wtf', 'what is a monkey wrench?', 'goodbye', 'i love you', 'hot hot hot!!', 'hell yeah']
 const vectors = []
 for (let i = 0; i < processedTweets.length; i++) {
     const tweet = processedTweets[i]
@@ -67,5 +68,14 @@ for (let i = 0; i < processedTweets.length; i++) {
     vectors.push({ tweet, vector })
 }
 
+import PCA from 'pca-js'
+// var data = [[40,50,60],[50,70,60],[80,70,90],[50,60,80]];
+var data = vectors.map(item => item.vector)
+var vectors2 = PCA.getEigenVectors(data);
+var percent = PCA.computePercentageExplained(vectors2,vectors2[0], vectors2[1], vectors2[2])
+console.log({ percent })
+// var adData = PCA.computeAdjustedData(data, vectors2[0])
+// console.log({ adData: adData.adjustedData })
 
-await fs.promises.writeFile('vectors.json', JSON.stringify(vectors));
+// console.log(vectors2)
+await fs.promises.writeFile('vectors_2.json', JSON.stringify(vectors));
